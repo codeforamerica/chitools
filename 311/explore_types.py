3f5220da8744c53b2970f75a791b45ebb5e38e3a
Rob Brackett <rob@robbrackett.com>
2012-03-31 23:50:44 -0700

    Add methods for extracting the exact case sequences seen in the Socrata 311 data

--- 311/explore_types.py
+++ 311/explore_types.py
@@ -2,10 +2,16 @@
 Tools for exploring the tree of possible follow-ons for any given request type or set of request types.
 """
 
+import pymongo
+
+DB_HOST = 'localhost'
+DB_PORT = 27017
+DB_NAME = 'TestData'
+
 def check_deepness(db, types, seen_types=None):
   """
   Recursively identify request types that result as follow-ons from a given set of initial request types.
-  db = The name of the database to use
+  db = A database connection to use
   types = A list of request type names (not codes) to start from
   """
   overall_count = db.ServiceRequestsCombined.find({'service_request_type': {'$in': types}}).count()
@@ -45,10 +51,48 @@ def check_deepness(db, types, seen_types=None):
   }
 
 
+def extract_type_tree(db, types):
+  """
+  Identify all request types that can result from a given set of initial request types.
+  Unlike check_deepness, which identifies any type that *ever* result from a previous 
+  type in the sequence, this only identifies types that belong to an *existing* 
+  sequence of follow-ons starting with one of the initial types.
+  db = A database connection to use
+  types = A list of request type names (not codes) to start from
+  """
+  followed_types = {}
+  following_types = []
+  result_trees = {}
+  request_count = 0
+  for case in db.Cases.find({'initial_type': {'$in': types}}):
+    case_type = case['initial_type']
+    request_count += 1
+    tree = [case_type]
+    for index, request in enumerate(case['requests']):
+      if index > 0:
+        followed_types[case_type] = True
+        service_type = request['service_request_type']
+        if len(following_types) < index:
+          following_types.append({})
+        following_types[index - 1][service_type] = True
+        tree.append(service_type)
+          
+    if len(tree) > 1:
+      tree_string = ' -> '.join(tree)
+      result_trees[tree_string] = tree_string in result_trees and result_trees[tree_string] + 1 or 1
+    
+  return {
+    'request_count': request_count,
+    'followed_types': followed_types.keys(),
+    'following_types': following_types,
+    'follow_trees': result_trees,
+  }
+
+
 def print_deepness(db, types, title=None, single_line=False):
   """
   Prettily prints the results of check_deepness.
-  db = The name of the database to use
+  db = A database connection to use
   types = A list of request type names (not codes) to start from
   title = A title to print before the results
   single_line = If true, print each follow-on type on its own line (instead of a comma-separated list)
@@ -77,8 +121,41 @@ def print_deepness(db, types, title=None, single_line=False):
   print '\n\n'
 
 
+def print_type_tree(db, types, title=None, single_line=False, max_trees=25):
+  """
+  Prettily prints the results of extract_type_tree.
+  db = A database connection to use
+  types = A list of request type names (not codes) to start from
+  title = A title to print before the results
+  single_line = If true, print each follow-on type on its own line (instead of a comma-separated list)
+  max_trees = List no more than this many possible request sequences
+  """
+  result = extract_type_tree(db, types)
+  if title:
+    print '%s\n%s'% (title, len(title) * '=')
+  else:
+    print '===================='
+    
+  print 'Types: %s (%s)' % (', '.join(types), result['request_count'])
+  for depth, types in enumerate(result['following_types']):
+    space = (depth + 1) * '  '
+    line_start = '%sFollowed by: ' % space
+    follow_join = single_line and ('\n%s' % (len(line_start) * ' ')) or ', '
+    print '%s%s' % (line_start, follow_join.join(types.keys()))
+  
+  tree_count = len(result['follow_trees'])
+  if tree_count and max_trees:
+    print 'Possible paths:'
+    trees = sorted(result['follow_trees'].iteritems(), key=lambda x: x[1], reverse=True)
+    for tree, count in trees[:max_trees]:
+      print '  (%s) %s' % (count, tree)
+    if tree_count > max_trees:
+      '...and %s more' % (tree_count - max_trees)
+    
+  print '\n\n'
 
-types_animal = type_names = ["Vicious Animal",
+
+types_animal = ["Vicious Animal",
 "Stray Animal",
 "Animal Bite",
 "Animal Abandoned",
@@ -108,14 +185,29 @@ types_traffic_lights = ["Traffic Light Out","Red Light Camera","Traffic Light St
 
 types_building = ["Building Violation","No Building Permit & Construction Violations","Building - Illegal Conversion",]
 
+
 if __name__ == '__main__':
-  db = '311Data'
-  print_deepness(db, types_animal, 'ANIMALS')
-  print_deepness(db, types_rodent, 'RODENT')
-  print_deepness(db, types_trees, 'TREES')
-  print_deepness(db, types_graffiti, 'GRAFFITI')
-  print_deepness(db, types_dumping, 'DUMPING')
-  print_deepness(db, types_street_lights, 'STREET LIGHTS')
-  print_deepness(db, types_traffic_lights, 'TRAFFIC LIGHTS')
-  print_deepness(db, types_sewer, 'SEWER', True)
-  print_deepness(db, types_building, 'BUILDING', True)
+  connection = pymongo.Connection(DB_HOST, DB_PORT)
+  db = connection[DB_NAME]
+  
+  # Identify broad type connections
+  # print_deepness(db, types_animal, 'ANIMALS')
+  # print_deepness(db, types_rodent, 'RODENTS')
+  # print_deepness(db, types_trees, 'TREES')
+  # print_deepness(db, types_graffiti, 'GRAFFITI')
+  # print_deepness(db, types_dumping, 'DUMPING')
+  # print_deepness(db, types_street_lights, 'STREET LIGHTS')
+  # print_deepness(db, types_traffic_lights, 'TRAFFIC LIGHTS')
+  # print_deepness(db, types_sewer, 'SEWER', True)
+  # print_deepness(db, types_building, 'BUILDING', True)
+  
+  # Keep it to explicit trees of requests that have actually been seen
+  print_type_tree(db, types_animal, 'ANIMALS')
+  print_type_tree(db, types_rodent, 'RODENTS')
+  print_type_tree(db, types_trees, 'TREES')
+  print_type_tree(db, types_graffiti, 'GRAFFITI')
+  print_type_tree(db, types_dumping, 'DUMPING')
+  print_type_tree(db, types_street_lights, 'STREET LIGHTS')
+  print_type_tree(db, types_traffic_lights, 'TRAFFIC LIGHTS')
+  print_type_tree(db, types_sewer, 'SEWER', True)
+  print_type_tree(db, types_building, 'BUILDING', True)
