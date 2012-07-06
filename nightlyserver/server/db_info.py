ab139d8837c815b534de39dd8c03483a767b3cfe
Rob Brackett <rob@robbrackett.com>
2012-07-06 16:50:12 -0700

    Pull out database collection info into its own file so multiple modules can access it

---
+++ nightlyserver/server/db_info.py
@@ -0,0 +1,6 @@
+# This info is needed in multiple places
+COLLECTION_CASE_INDEX = 'CaseIndex'
+COLLECTION_CASES      = 'Cases'
+COLLECTION_ORPHANS    = 'OrphanCases'
+
+COLLECTION_SERVICES   = 'Services'
