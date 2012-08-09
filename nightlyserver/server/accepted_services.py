291ed4e19d6ba93314295c3743d737e936b6c15b
Rob Brackett <rob@robbrackett.com>
2012-08-09 12:08:02 -0700

    Updated the service type whitelist to include two types from the initial 14 that will be released that weren't in the top 50.

--- nightlyserver/server/accepted_services.py
+++ nightlyserver/server/accepted_services.py
@@ -1,2 +1,4 @@
 # The top 50 types that will initially be exposed
-ACCEPTED_SERVICES = ('GRAF', 'SIE', 'SFE', 'TBC', 'PHF', 'SGA', 'BBA', 'SKA', 'EAE', 'SCB', 'SEC', 'SEF', 'SEL', 'TBD', 'SFA', 'EAF', 'EAQ', 'SGQ', 'AAF', 'WBT', 'CHECKFOR', 'SFB', 'CSC', 'WM3', 'BBK', 'SCP', 'PBE', 'BBC', 'SEE', 'AAE', 'SDR', 'EAB', 'PBS', 'SFD', 'WB9', 'BBD', 'SCC', 'BBB', 'AAC', 'PBD', 'EAK', 'AAD', 'EAD', 'WBJ', 'PCE', 'BBE', 'PCB', 'SIL', 'SDP', 'PBH')
+ACCEPTED_SERVICES = ('GRAF', 'SIE', 'SFE', 'TBC', 'PHF', 'SGA', 'BBA', 'SKA', 'EAE', 'SCB', 'SEC', 'SEF', 'SEL', 'TBD', 'SFA', 'EAF', 'EAQ', 'SGQ', 'AAF', 'WBT', 'CHECKFOR', 'SFB', 'CSC', 'WM3', 'BBK', 'SCP', 'PBE', 'BBC', 'SEE', 'AAE', 'SDR', 'EAB', 'PBS', 'SFD', 'WB9', 'BBD', 'SCC', 'BBB', 'AAC', 'PBD', 'EAK', 'AAD', 'EAD', 'WBJ', 'PCE', 'BBE', 'PCB', 'SIL', 'SDP', 'PBH', 
+# Additional initial 14 types:
+'HFB', 'PHQ')
